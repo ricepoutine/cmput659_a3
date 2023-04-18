3ae05f3fe700333b86b3e8e46f841f4d5902732a
ricepoutine <dutx99@gmail.com>
2023-04-17 21:43:14 -0600

    test suite working

--- starter/main_latent_search.py
+++ starter/main_latent_search.py
@@ -5,10 +5,13 @@ from logger.stdout_logger import StdoutLogger
 from vae.models import load_model
 from search.latent_search import LatentSearch
 from tasks import get_task_cls
+import csv
+import time
 
+def trial():
 
-if __name__ == '__main__':
-    
+    print(f"Beginning with trial with seed:", Config.env_seed)
+
     dsl = DSL.init_default_karel()
 
     if Config.disable_gpu:
@@ -27,9 +30,11 @@ if __name__ == '__main__':
     
     StdoutLogger.log('Main', f'Starting Latent Search with model {Config.model_name} for task {Config.env_task}')
     
+    st = time.process_time()
+
     best_program, best_reward = searcher.search()
     #best_program, best_reward = searcher.search_better()
-    searcher.test_suite(10)
+    et = time.process_time()
 
     #optional: look at gifs
     #searcher.save_gifs(best_program)
@@ -37,5 +42,32 @@ if __name__ == '__main__':
     StdoutLogger.log('Main', f'Final program: {best_program}') 
     StdoutLogger.log('Main', f'Reward of Final Program: {best_reward}') 
 
+    with open(
+        "output/results/" + f"{Config.search_type}_{Config.search_number_iterations}" + ".csv", "a", newline=""
+    ) as csvfile:
+        logger = csv.writer(csvfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+        logger.writerow([Config.env_seed, str(et-st), best_reward.item(), best_program])
+
+
+
+if __name__ == '__main__':
+    #main experiment control
+
+    num_trials = 20 #20 = number of seeds, used to report average best program and error
+    search_iterations = [10, 20] #[10, 20, 30, 40, 50]
+
+    for num_iterations in search_iterations:
+        Config.search_number_iterations = num_iterations
+        with open(
+            "output/results/" + f"{Config.search_type}_{Config.search_number_iterations}" + ".csv", "w+", newline=""
+        ) as csvfile:
+            logger = csv.writer(csvfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+            logger.writerow(["Seed:", "Runtime:", "Reward of Final Program:", "Final program:"])
+
+        for seed in range(1,num_trials+1): # just to make counting seeds easier, in case more seeds are wanted
+            Config.env_seed = seed
+            trial()
+
+
 # for ease of access: 
-# python main_latent_search.py --env_seed 1 --disable_gpu --search_number_iterations 20 --search_population_size 1028 --env_is_crashable
+# python main_latent_search.py --env_seed 1 --search_number_iterations 20 --search_population_size 1028 --env_is_crashable
